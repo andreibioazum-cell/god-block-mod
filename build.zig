e3475fb3f021af06041696bfffe87b7a5b43defa
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-18 09:40:26 +0400

    Update build.zig

--- build.zig
+++ build.zig
@@ -4,7 +4,6 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // Создаем общую библиотеку для Android
     const lib = b.addSharedLibrary(.{
         .name = "CubicBattle",
         .root_source_file = b.path("main.zig"),
@@ -12,13 +11,11 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    // Подключаем Sokol
-    const dep_sokol = b.dependency("sokol", .{
+    const sokol = b.dependency("sokol", .{
         .target = target,
         .optimize = optimize,
     });
-    lib.root_module.addImport("sokol", dep_sokol.module("sokol"));
+    lib.root_module.addImport("sokol", sokol.module("sokol"));
 
-    // Это нужно, чтобы артефакт попал в zig-out/lib
     b.installArtifact(lib);
 }
