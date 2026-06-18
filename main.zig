df5a0b00c4a71775f3912620a968846a98b12638
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-18 09:33:23 +0400

    Update main.zig

--- main.zig
+++ main.zig
@@ -3,6 +3,11 @@ const sokol = @import("sokol");
 const sapp = sokol.app;
 const sg = sokol.gfx;
 
+// Эта функция — точка входа для Android NativeActivity
+pub export fn ANativeActivity_onCreate(activity: anytype, saved_state: anytype, len: usize) void {
+    _ = activity; _ = saved_state; _ = len;
+}
+
 pub fn main() void {
     sapp.run(.{
         .init_cb = init,
@@ -20,9 +25,8 @@ fn init() void {
 
 fn frame() void {
     var pass_action = sg.PassAction{};
-    pass_action.colors[0] = .{ .load_action = .CLEAR, .clear_value = .{ .r = 0.2, .g = 0.6, .b = 0.9, .a = 1.0 } };
+    pass_action.colors[0] = .{ .load_action = .CLEAR, .clear_value = .{ .r = 0.1, .g = 0.6, .b = 0.8, .a = 1.0 } };
     sg.beginPass(.{ .action = pass_action, .swapchain = sapp.swapchain() });
-    // Тут будет твой воксельный движок
     sg.endPass();
     sg.commit();
 }
