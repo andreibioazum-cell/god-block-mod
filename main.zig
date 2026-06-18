3cc6e58bc65e09355a24213ff2cf91cff1a8d5c9
andreibioazum-cell <andreibioazum@gmail.com>
2026-06-18 09:37:32 +0400

    Update main.zig

--- main.zig
+++ main.zig
@@ -3,11 +3,6 @@ const sokol = @import("sokol");
 const sapp = sokol.app;
 const sg = sokol.gfx;
 
-// Эта функция — точка входа для Android NativeActivity
-pub export fn ANativeActivity_onCreate(activity: anytype, saved_state: anytype, len: usize) void {
-    _ = activity; _ = saved_state; _ = len;
-}
-
 pub fn main() void {
     sapp.run(.{
         .init_cb = init,
@@ -15,7 +10,7 @@ pub fn main() void {
         .cleanup_cb = cleanup,
         .width = 1280,
         .height = 720,
-        .window_title = "Zig Cubic Battle",
+        .window_title = "Zig Cubic",
     });
 }
 
@@ -25,7 +20,11 @@ fn init() void {
 
 fn frame() void {
     var pass_action = sg.PassAction{};
-    pass_action.colors[0] = .{ .load_action = .CLEAR, .clear_value = .{ .r = 0.1, .g = 0.6, .b = 0.8, .a = 1.0 } };
+    pass_action.colors[0] = .{ 
+        .load_action = .CLEAR, 
+        .clear_value = .{ .r = 0.1, .g = 0.5, .b = 0.9, .a = 1.0 } 
+    };
+    
     sg.beginPass(.{ .action = pass_action, .swapchain = sapp.swapchain() });
     sg.endPass();
     sg.commit();
